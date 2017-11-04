880fe60ee11642de89b54877393171fba8b3dad6
Szczepan Faber <szczepiq@gmail.com>
2017-11-04 14:20:29 -0700

    1.2.2 code
    Note that one existing test method is failing

--- src/main/java/org/workshop/FlightManager.java
+++ src/main/java/org/workshop/FlightManager.java
@@ -1,20 +1,38 @@
 package org.workshop;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import static java.util.Arrays.asList;
+
 public class FlightManager {
-    private Map<String, Integer> flights = new HashMap<>();
+    private Map<String, List<Seat>> flights = new HashMap<>();
 
     public void addFlight(String flightNo, int seats) {
-        flights.put(flightNo, seats);
+
     }
 
     public int getAvailableSeats(String flightNo) {
-        Integer seats = flights.get(flightNo);
+        List<Seat> seats = flights.get(flightNo);
         if (seats == null) {
             throw new FlightNotFoundException();
         }
-        return seats;
+        return seats.size();
+    }
+
+    public void addFlight(String flightNo, Seat ... seats) {
+        this.flights.put(flightNo, asList(seats));
+    }
+
+    public Seat getCheapestSeat(String flightNo) {
+        List<Seat> seats = flights.get(flightNo);
+        Seat cheapest = null;
+        for (Seat seat : seats) {
+            if (cheapest == null || seat.getPrice() < cheapest.getPrice()) {
+                cheapest = seat;
+            }
+        }
+        return cheapest;
     }
 }
---
+++ src/main/java/org/workshop/Seat.java
@@ -0,0 +1,13 @@
+package org.workshop;
+
+public class Seat {
+    private final int price;
+
+    public Seat(int price) {
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
