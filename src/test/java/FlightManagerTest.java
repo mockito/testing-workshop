b8fd5ad15ceaff71ab809962813627b0c47ea5cc
sfaber <sfaber@linkedin.com>
2020-10-09 15:21:54 -0500

    Refactoring

--- src/test/java/FlightManagerTest.java
+++ src/test/java/FlightManagerTest.java
@@ -5,10 +5,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FlightManagerTest {
 
+    FlightManager manager = new FlightManager();
+
     @Test
     public void should_show_available_seats() {
         //given
-        FlightManager manager = new FlightManager();
         manager.addFlight("LH123", new Seat(100), new Seat(100));
         manager.addFlight("AA900", new Seat(200));
 
@@ -21,10 +22,6 @@ public class FlightManagerTest {
 
     @Test
     public void should_deal_with_missing_flight() {
-        //given
-        FlightManager manager = new FlightManager();
-        manager.addFlight("LH123", new Seat(100));
-
         //expect
         assertThatThrownBy(
                 () -> manager.getFlight("AA900"))
@@ -34,7 +31,6 @@ public class FlightManagerTest {
     @Test
     public void should_show_cheapest_seat() {
         //given
-        FlightManager manager = new FlightManager();
         manager.addFlight("LH123", new Seat(100), new Seat(50));
         manager.addFlight("LH500", new Seat(30));
 
